6f5e467a6df1a7e2531700fe094e1fd351626f08
rifkiansyahmc <skyline_bell_cat@yahoo.com>
2014-03-06 17:08:50 +0700

    initial commit: empty main.cpp

---
+++ jockeyingqueue/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: ACER
+ *
+ * Created on March 6, 2014, 1:57 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
---
+++ jockeyingqueue/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=MinGW-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/MinGW-Windows
+CND_ARTIFACT_NAME_Debug=jockeyingqueue
+CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW-Windows/jockeyingqueue
+CND_PACKAGE_DIR_Debug=dist/Debug/MinGW-Windows/package
+CND_PACKAGE_NAME_Debug=jockeyingqueue.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/MinGW-Windows/package/jockeyingqueue.tar
+# Release configuration
+CND_PLATFORM_Release=MinGW-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/MinGW-Windows
+CND_ARTIFACT_NAME_Release=jockeyingqueue
+CND_ARTIFACT_PATH_Release=dist/Release/MinGW-Windows/jockeyingqueue
+CND_PACKAGE_DIR_Release=dist/Release/MinGW-Windows/package
+CND_PACKAGE_NAME_Release=jockeyingqueue.tar
+CND_PACKAGE_PATH_Release=dist/Release/MinGW-Windows/package/jockeyingqueue.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
